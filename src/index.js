6ee968ce544ebb1bd2a2014ae74ac4da92226ed3
Bruno Luiz <contact@brunoluiz.net>
2017-12-27 22:43:26 -0200

    Remove service worker references

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './index.css'
 import 'normalize.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import registerServiceWorker from './registerServiceWorker'
 import App from './containers/App.jsx'
 import store from './store'
 import { Provider } from 'react-redux'
@@ -13,5 +12,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// registerServiceWorker()
