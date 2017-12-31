c3c3184bf2b7cf3d5408032964faf7ae333c1fe3
Bruno Luiz <contact@brunoluiz.net>
2017-12-31 11:18:08 -0200

    Add Loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { Spin } from 'antd'
+
+import style from './style.module.css'
+
+export default () =>
+  <div className={style.loading}>
+    <Spin size='large' />
+  </div>
