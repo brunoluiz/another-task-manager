60141673cd1a1632839bbe75ee8bbb6d216712ae
Bruno Luiz <contact@brunoluiz.net>
2017-12-30 22:16:44 -0200

    If a new user log-in, create a users entry for

--- src/modules/auth/operations.js
+++ src/modules/auth/operations.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import users from '../../repositories/users-repository'
 import * as types from './types'
 import {
   call,
@@ -10,15 +11,24 @@ const provider = new firebase.auth.GoogleAuthProvider()
 
 export function* auth () {
   try {
-    const { user } = yield firebase.auth().signInWithPopup(provider)
+    const {
+      additionalUserInfo,
+      user
+    } = yield firebase.auth().signInWithPopup(provider)
+
     const data = {
       name: user.displayName,
       email: user.email,
-      pic: user.photoURL,
-      id: user.uid
+      photoURL: user.photoURL,
+      id: user.uid,
+      profile: additionalUserInfo.profile
     }
 
     yield put({ type: types.AUTH_SUCCESS, data })
+
+    if (additionalUserInfo.isNewUser) {
+      yield users.save(data)
+    }
   } catch (e) {
     yield put({ type: types.AUTH_FAILURE })
   }
